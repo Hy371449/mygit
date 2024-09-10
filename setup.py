d4f854e4d0e2f1c93cf03109bc7b15e4b5fee5fb
huangyue <jon371449@gmail.com>
2024-09-10 10:27:09 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mygit',
+      version='1.0',
+      packages=['mygit'],
+      entry_points={
+          'console_scripts': ['mygit=mygit.cli:main']
+      })
