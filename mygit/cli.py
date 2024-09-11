aadce6b05d2ebc3c0dc889466d6ca871fc21f5f8
huangyue <jon371449@gmail.com>
2024-09-11 11:37:43 -0700

    Commit
    Commit
    
    Commit
    
        pick e3e956a Commit
        pick 18b37e7 Commit

--- mygit/cli.py
+++ mygit/cli.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import textwrap
 
 from . import base
 from . import data
@@ -35,6 +36,23 @@ def parse_args():
     read_tree_parser.set_defaults(func=read_tree)
     read_tree_parser.add_argument('tree')
 
+    commit_parser = commands.add_parser('commit')
+    commit_parser.set_defaults(func=commit)
+    commit_parser.add_argument('-m', '--message', required=True)
+
+    log_parser = commands.add_parser('log')
+    log_parser.set_defaults(func=log)
+    log_parser.add_argument('oid', default='@', type=oid, nargs='?')
+
+    checkout_parser = commands.add_parser('checkout')
+    checkout_parser.set_defaults(func=checkout)
+    checkout_parser.add_argument('oid', type=oid)
+
+    tag_parser = commands.add_parser('tag')
+    tag_parser.set_defaults(func=tag)
+    tag_parser.add_argument('name')
+    tag_parser.add_argument('oid', default='@', type=oid, nargs='?')
+
     return parser.parse_args()
 
 
@@ -59,3 +77,27 @@ def write_tree(args):
 
 def read_tree(args):
     base.read_tree(args.tree)
+
+
+def commit(args):
+    print(base.commit(args.message))
+
+
+def log(args):
+    oid = args.oid
+    while oid:
+        commit = base.get_commit(oid)
+        print(f'commit {oid}\n')
+        print(textwrap.indent(commit.message, '    '))
+        print('')
+
+        oid = commit.parent
+
+
+def checkout(args):
+    base.checkout(args.oid)
+
+
+def tag(args):
+    base.create_tag(args.name, args.oid)
+
